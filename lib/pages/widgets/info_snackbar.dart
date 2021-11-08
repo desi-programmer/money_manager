19630c639a838a24d81ef52d59402a7133591b8a
Prince <knightsilent2422@gmail.com>
2021-11-08 20:50:04 +0530

    Added a Snackbar to display info on Delete

---
+++ lib/pages/widgets/info_snackbar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:expense/static.dart' as Static;
+
+SnackBar deleteInfoSnackBar = SnackBar(
+  backgroundColor: Static.PrimaryColor,
+  duration: Duration(
+    seconds: 2,
+  ),
+  content: Row(
+    children: [
+      Icon(
+        Icons.info,
+        color: Colors.white,
+      ),
+      SizedBox(
+        width: 6.0,
+      ),
+      Text(
+        "Long Press to delete",
+        style: TextStyle(
+          fontSize: 16.0,
+        ),
+      ),
+    ],
+  ),
+);
